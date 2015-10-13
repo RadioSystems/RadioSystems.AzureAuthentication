d03207a1dbcc8421dd4e20817ff700e9482a332f
Westley Harris <wharris@petsafe.net>
2015-10-13 07:28:33 -0400

    Fixed some references. And fixed the default azure tenant.
    Signed-off-by: Westley Harris <wharris@petsafe.net>

--- Enums/DefaultAzureSettings.cs
+++ Enums/DefaultAzureSettings.cs
@@ -2,7 +2,7 @@
     public sealed class DefaultAzureSettings {
         public static readonly string Tenant = "https://mytenant.com";
         public static readonly string ClientId = "MyClientId";
-        public static readonly string ADInstance = "https://login.microsoftonline.com/{0}";
+        public static readonly string ADInstance = "https://login.microsoft.com/{0}";
         public static readonly string AppName = "MyAppName";
         public static readonly string LogoutRedirectUri = "http://localhost:30321/OrchardLocal/";
         public static readonly bool BearerAuthEnabled = false;
