038e920b951e7722a95a62e1d4c2e41de34b7aa6
Westley Harris <wharris@petsafe.net>
2015-10-18 14:15:48 -0400

    Using ISiteService to access azure settings. Including using ISite.baseUrl as default logout redirect uri. Fixes #4

--- OwinMiddlewares.cs
+++ OwinMiddlewares.cs
@@ -1,72 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.IdentityModel.Tokens;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using System.Web.Helpers;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.ActiveDirectory;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.DataProtection;
 using Microsoft.Owin.Security.OpenIdConnect;
-using Orchard;
-using Orchard.Settings;
 using Orchard.ContentManagement;
-using Orchard.Data;
 using Orchard.Logging;
 using Orchard.Owin;
+using Orchard.Settings;
 using Owin;
-using RadioSystems.AzureAuthentication.Enums;
 using RadioSystems.AzureAuthentication.Models;
 using RadioSystems.AzureAuthentication.Security;
 
 namespace RadioSystems.AzureAuthentication {
     public class OwinMiddlewares : IOwinMiddlewareProvider {
         public ILogger Logger { get; set; }
-
-        private readonly ISiteService _siteService;
-
-        private static readonly string _defaultTenant = "https://mytenant.com";
-        private static readonly string _defaultClientId = "MyClientId";
-        private static readonly string _defaultADInstance = "https://login.microsoftonline.com/{0}";
-        private static readonly string _defaultAppName = "MyAppName";
+       
+        private readonly string _azureClientId;
+        private readonly string _azureTenant;
+        private readonly string _azureADInstance;
+        private readonly string _logoutRedirectUri;
+        private readonly string _azureAppName;
+        private readonly bool _sslEnabled;
+        private readonly bool _azureWebSiteProtectionEnabled;
 
         public OwinMiddlewares(ISiteService siteService) {
             Logger = NullLogger.Instance;
 
-            _siteService = siteService;
+            var site = siteService.GetSiteSettings();
+            var azureSettings = site.As<AzureSettingsPart>();
+
+            _azureClientId = ((azureSettings.ClientId == null) || (azureSettings.ClientId == string.Empty)) ? 
+                "[example: 82692da5-a86f-44c9-9d53-2f88d52b478b]" : azureSettings.ClientId;
+
+            _azureTenant = ((azureSettings.Tenant == null) || (azureSettings.Tenant == string.Empty)) ? 
+                "faketenant.com" : azureSettings.Tenant;
+
+            _azureADInstance = ((azureSettings.ADInstance == null) || (azureSettings.ADInstance == string.Empty)) ? 
+                "https://login.microsoft.com/{0}" : azureSettings.ADInstance;
+
+            _logoutRedirectUri = ((azureSettings.LogoutRedirectUri == null) || (azureSettings.LogoutRedirectUri == string.Empty)) ? 
+                site.BaseUrl : azureSettings.LogoutRedirectUri;
+
+            _azureAppName = ((azureSettings.AppName == null) || (azureSettings.AppName == string.Empty)) ? 
+                "[example: MyAppName]" : azureSettings.AppName;
+
+            _sslEnabled = azureSettings.SSLEnabled;
+
+            _azureWebSiteProtectionEnabled = azureSettings.AzureWebSiteProtectionEnabled;
         }
 
         public IEnumerable<OwinMiddlewareRegistration> GetOwinMiddlewares() {
             var middlewares = new List<OwinMiddlewareRegistration>();
 
-            var site = _siteService.GetSiteSettings();
-
-            var settings = site.As<AzureSettingsPart>();
-
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
 
             var openIdOptions = new OpenIdConnectAuthenticationOptions {
-                ClientId = settings.ClientId ?? _defaultClientId,
-                Authority = string.Format(CultureInfo.InvariantCulture, settings.ADInstance ?? _defaultADInstance, settings.Tenant ?? _defaultTenant),
-                PostLogoutRedirectUri = settings.LogoutRedirectUri ?? site.BaseUrl,
-                Notifications = new OpenIdConnectAuthenticationNotifications()
+                ClientId = _azureClientId,
+                Authority = string.Format(CultureInfo.InvariantCulture, _azureADInstance, _azureTenant),
+                PostLogoutRedirectUri = _logoutRedirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications ()
             };
 
             var cookieOptions = new CookieAuthenticationOptions();
 
             var bearerAuthOptions = new WindowsAzureActiveDirectoryBearerAuthenticationOptions {
-                //TODO: set this to https if ssl enabled settings is true
                 TokenValidationParameters = new TokenValidationParameters {
-                    ValidAudience = string.Format("http://{0}/{1}", settings.Tenant ?? _defaultTenant, settings.AppName ?? _defaultAppName)
-                },
-                Tenant = _defaultTenant,
-                AuthenticationType = "Oauth2Bearer"
+                    ValidAudience = string.Format(_sslEnabled ? "https://{0}/{1}" : "http://{0}/{1}", _azureTenant, _azureAppName)
+                }
             };
 
-            if (_defaultWebSiteProtectionEnabled) {
+            if (_azureWebSiteProtectionEnabled) {
                 middlewares.Add(new OwinMiddlewareRegistration {
                     Priority = "9",
                     Configure = app => { app.SetDataProtectionProvider(new MachineKeyProtectionProvider()); }
